66d3d8e7858f7111a79122e11fc1fdf7a041fb5f
ryuki1126 <uv.ryuki1126@gmail.com>
2022-03-17 10:33:08 +0900

    複数デバイスのオンオフを同時切替できるよう修正

--- index.py
+++ index.py
@@ -1,28 +1,35 @@
+from time import sleep
 import requests
 import json
 
-device_id = 'A848FAE3E817'
-access_token = 'f2f6da9670e143602b2de7e972dedb042dc4e28c6aaa00fcbfa77976345806f261e436f362b8775c37c6aaee77efa597'
+# 一斉にオンオフさせるswitchBotのデバイスIDを配列に格納する
+device_ids = [
+  'deviced_id1',
+  'device_id2'
+]
+access_token = 'token'
 api_base_url = 'https://api.switch-bot.com'
 api_on = 'turnOn'
 api_off = 'turnOff'
 
+# 配列に格納したデバイスIDを持つswitchBotを全てオン又はオフにする
 def device_control(api_command):
   headers = {
     'Content-Type': 'application/json; charset: utf8',
     'Authorization': access_token
   }
-  url = api_base_url + '/v1.0/devices/' + device_id + '/commands'
   body = {
-    'command': api_command,
-    'parameter': 'default',
-    'commandType': 'command'
+  'command': api_command,
+  'parameter': 'default',
+  'commandType': 'command'
   }
-  ddd = json.dumps(body)
-  print(ddd)
-  
-  res = requests.post(url, data=ddd, headers=headers)
-  print(res)
+  for device_id in device_ids:
+    url = api_base_url + '/v1.0/devices/' + device_id + '/commands'
+    ddd = json.dumps(body)
+    res = requests.post(url, data=ddd, headers=headers)
+    print(res)
 
+
+device_control(api_on)
+sleep(10)
 device_control(api_off)
-
